cee55e2d635a8d8b98f7eb804999470ac53db871
kijimoshi1337 <502234iw@gmail.com>
2025-02-24 10:37:57 +0100

    port fixed

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def preview_file(filename):
 
 if __name__ == '__main__':
     scheduler.start()
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
